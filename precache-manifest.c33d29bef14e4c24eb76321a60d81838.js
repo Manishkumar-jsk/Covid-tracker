d494ecc3a5a08b23fc022b840d1fbaa2e0caaf17
Manishkumar-jsk <Manishsharma65263@gmail.com>
2021-12-23 12:50:52 +0530

    Updates

---
+++ precache-manifest.c33d29bef14e4c24eb76321a60d81838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d985106e6926f39d203e5cfce1ea1e35",
+    "url": "/Covid-tracker/index.html"
+  },
+  {
+    "revision": "56891d37318ba7689123",
+    "url": "/Covid-tracker/static/css/main.cda3f348.chunk.css"
+  },
+  {
+    "revision": "3ab05b57b6c7c49ef54c",
+    "url": "/Covid-tracker/static/js/2.e3af2233.chunk.js"
+  },
+  {
+    "revision": "0a88e5e3a9f2b73763f073064b14c7f9",
+    "url": "/Covid-tracker/static/js/2.e3af2233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56891d37318ba7689123",
+    "url": "/Covid-tracker/static/js/main.d60b8d8b.chunk.js"
+  },
+  {
+    "revision": "dd7ba23e447d1530d345",
+    "url": "/Covid-tracker/static/js/runtime-main.42ffeb29.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-tracker/static/media/covid.d7265326.png"
+  }
+]);
